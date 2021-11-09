0f2ed24007c82ba51b028542731bee9249701658
Adam <cloudflips321@icloud.com>
2021-11-08 20:54:13 -0500

    Homescreen, screens folder created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <>
     <Header />
     <main className="py-3">
-    <Container>
-      <h1>Welcome to ProShop!</h1>
-    </Container>
+      <Container>
+        <Homescreen />
+      </Container>
     </main>
     <Footer />
     </>
