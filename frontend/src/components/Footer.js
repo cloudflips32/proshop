a122b6b56d65adf800d8f6802e1c36974dc6d29e
Adam <cloudflips321@icloud.com>
2021-11-08 19:52:35 -0500

    React-Bootstrap Setup, Header/Footer Components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+    <Container>
+      <Row>
+        <Col>
+        <h2>This is the footer.</h2>
+        </Col>
+      </Row>
+    </Container>
+    </footer>
+  )
+}
+
+export default Footer
