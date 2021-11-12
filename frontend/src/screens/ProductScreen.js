2d54f58bb214d2cae5dfb8754e7281ea14703923
Adam <cloudflips321@icloud.com>
2021-11-12 10:02:59 -0500

    added nodemon, concurrently; Homescreen and ProductScreen both fetch products from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+  fetchProduct()
+  }, [match])
 
   const product = products.find((p) => p._id === match.params.id)
   return (
