5678b285162a7d2260aa22b5a76b52eb72d797fd
Adam <cloudflips321@icloud.com>
2021-11-16 16:07:21 -0500

    create state for product deatils on productscreen using previous redux workflow

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'react-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 
 const initialState = {}
+
+const middleware = [thunk]
 
 const store = createStore(
 reducer, 
