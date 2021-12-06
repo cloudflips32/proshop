1f64b10298c327c54f7fd03aced719be20f5f762
Adam <cloudflips321@icloud.com>
2021-12-06 17:22:07 -0500

    a ton of debugging

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import listProducts from '../actions/productActions'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
 
 const Homescreen = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
-
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts())
-    }, [dispatch])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? ( <Loader /> ) : error ? ( <Message variant='danger'>{error}</Message> ) : 
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>}
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Homescreen
+export default Homescreen;
