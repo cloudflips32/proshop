71591f8a43a8b3b28926a3f11346c279a05f3088
Adam <cloudflips321@icloud.com>
2021-12-09 17:18:00 -0500

    attempted debug, added serviceworker, imported serviceworker into index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import store from "./store";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,3 +13,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+serviceWorker.unregister()
